4e7cdf15648e5049ae8b857a2628c19c86f34f17
Valera <valerylarionov06@gmail.com>
2026-09-19 20:50:02 +0300

    add \n end String New Feature!

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, world!\n");
-	System.out.println("New Feature!");
+	System.out.println("New Feature!\n");
     }
 }
